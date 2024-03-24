570e1264eb1faf9cd9256d00bd65203390e7650a
Jaweria Rashdi <rashdijaweria@gmail.com>
2024-03-24 13:44:15 +0500

    REMOVED NOTES

--- index.ts
+++ index.ts
@@ -2,10 +2,6 @@
 
 import inquirer from "inquirer"; 
 
-// 1> Computer will generate a random number
-// 2> user input for guessing number
-// 3> compare user input with computer generated number and show result
- 
 const randomNumber = Math.floor(Math.random() * 6 + 1);
 const answers = await inquirer.prompt([
     {
